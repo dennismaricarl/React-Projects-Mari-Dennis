658bf669551db1ee020763acff384cc8b93c01cc
Mari <dennismaricarl@gmail.com>
2024-03-12 12:19:01 -0700

    initial set up

---
+++ traffic-light/src/App.js
@@ -0,0 +1,12 @@
+import TrafficLight from './TrafficLight';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <TrafficLight/>
+    </div>
+  );
+}
+
+export default App;
---
+++ traffic-light/src/TrafficLight.js
@@ -0,0 +1,9 @@
+function TrafficLight(){
+    return(
+        <>
+        <h1>HELLO!!!!!!!!SET UP</h1>
+        </>
+    )
+}
+
+export default TrafficLight
