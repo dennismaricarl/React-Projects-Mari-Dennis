c5fe87a1f05c1502d90a246cbfb93fec81e39442
Mari <dennismaricarl@gmail.com>
2024-03-13 15:36:31 -0700

    CSS edits + additional traffic light

--- traffic-light/src/App.js
+++ traffic-light/src/App.js
@@ -31,6 +31,7 @@ function App() {
       <TrafficLight trafficColors={trafficColors} layout="horizontal"/>
       <TrafficLight trafficColors={trafficColors} layout="horizontal"/>
       <TrafficLight trafficColors={trafficColors} layout="horizontal"/>
+      <TrafficLight trafficColors={trafficColors} layout="horizontal"/>
     </div>
     </>
   );
